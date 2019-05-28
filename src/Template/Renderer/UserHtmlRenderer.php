75fb994e894657b578664b58c8357d6bf00a0cf0
Fayez Naccache <fayez.naccache@gmail.com>
2019-05-28 16:27:42 +0200

    add User renderer

---
+++ src/Template/Renderer/UserHtmlRenderer.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Evaneos\Template\Renderer;
+
+use Evaneos\Entity\User;
+
+class UserHtmlRenderer implements RendererInterface
+{
+    /**
+     * @param string $templateString
+     * @param User   $user
+     *
+     * @return string
+     */
+    public function render($templateString, $user)
+    {
+        return str_replace('[user:first_name]', ucfirst(mb_strtolower($user->getFirstname())), $templateString);
+    }
+
+    /**
+     * @param string $class
+     *
+     * @return bool
+     */
+    public function supports($class)
+    {
+        return User::class === $class;
+    }
+}
--- src/TemplateManager.php
+++ src/TemplateManager.php
@@ -2,22 +2,18 @@
 
 namespace Evaneos;
 
-use Evaneos\Entity\Destination;
 use Evaneos\Entity\Quote;
 use Evaneos\Entity\Template;
 use Evaneos\Entity\User;
-use Evaneos\Repository\DestinationRepository;
-use Evaneos\Repository\QuoteRepository;
-use Evaneos\Repository\SiteRepository;
 use Evaneos\Template\Renderer\RendererFactory;
 use Evaneos\Template\Renderer\RendererInterface;
+use Evaneos\Template\Renderer\RendererNotFoundexception;
 
 class TemplateManager
 {
     const TARGET_ENTITIES = [
         Quote::class,
-//        Destination::class,
-//        User::class
+        User::class
     ];
 
     /**
@@ -38,85 +34,25 @@ class TemplateManager
         $this->initRenderers();
     }
 
-    public function getTemplateComputed(Template $tpl, array $data)
+    public function getTemplateComputed(Template $template, array $data)
     {
-        if (!$tpl) {
-            throw new \RuntimeException('no tpl given');
+        if (!array_key_exists('user', $data)) {
+            $data['user'] = $this->currentUser;
         }
-
-        $replaced = clone($tpl);
 
         /* @var $renderer RendererInterface */
         foreach ($this->renderers as $renderer) {
             foreach ($data as $key => $entity) {
                 if ($renderer->supports(get_class($entity))) {
-                    $replaced
-                        ->setSubject($renderer->render($replaced->getSubject(), $entity))
-                        ->setContent($renderer->render($replaced->getContent(), $entity))
+                    $template
+                        ->setSubject($renderer->render($template->getSubject(), $entity))
+                        ->setContent($renderer->render($template->getContent(), $entity))
                     ;
                 }
             }
         }
 
-        return $replaced;
-    }
-
-    private function computeText($text, array $data)
-    {
-        /* @var $quote Quote */
-        $quote = (isset($data['quote']) and $data['quote'] instanceof Quote) ? $data['quote'] : null;
-
-        if ($quote)
-        {
-            $_quoteFromRepository = QuoteRepository::getInstance()->getById($quote->getId());
-            $site = SiteRepository::getInstance()->getById($quote->getSiteId());
-            $destinationOfQuote = DestinationRepository::getInstance()->getById($quote->getDestinationId());
-
-            if(strpos($text, '[quote:destination_link]') !== false){
-                $destination = DestinationRepository::getInstance()->getById($quote->getDestinationId());
-            }
-
-            $containsSummaryHtml = strpos($text, '[quote:summary_html]');
-            $containsSummary     = strpos($text, '[quote:summary]');
-
-            if ($containsSummaryHtml !== false || $containsSummary !== false) {
-                if ($containsSummaryHtml !== false) {
-                    $text = str_replace(
-                        '[quote:summary_html]',
-                        Quote::renderHtml($_quoteFromRepository),
-                        $text
-                    );
-                }
-                if ($containsSummary !== false) {
-                    $text = str_replace(
-                        '[quote:summary]',
-                        Quote::renderText($_quoteFromRepository),
-                        $text
-                    );
-                }
-            }
-
-            (strpos($text, '[quote:destination_name]') !== false) and $text = str_replace('[quote:destination_name]',$destinationOfQuote->getCountryName(),$text);
-        }
-
-        /* @var $destination Destination */
-        if (isset($destination))
-            $text = str_replace('[quote:destination_link]', $site->getUrl() . '/' . $destination->getCountryName() . '/quote/' . $_quoteFromRepository->getId(), $text);
-        else
-            $text = str_replace('[quote:destination_link]', '', $text);
-
-        /*
-         * USER
-         * [user:*]
-         */
-
-        /* @var $user User */
-        $user  = (isset($data['user'])  and ($data['user']  instanceof User))  ? $data['user']  : $this->currentUser;
-        if($user) {
-            (strpos($text, '[user:first_name]') !== false) and $text = str_replace('[user:first_name]'       , ucfirst(mb_strtolower($user->getFirstname())), $text);
-        }
-
-        return $text;
+        return $template;
     }
 
     private function initRenderers()
@@ -124,7 +60,11 @@ class TemplateManager
         /* @var $renderer RendererInterface */
         foreach ($this->targetEntities as $entityFqcn) {
             foreach (RendererFactory::FORMATS as $format) {
-                $this->renderers[] = RendererFactory::get($entityFqcn, $format);
+                try {
+                    $this->renderers[] = RendererFactory::get($entityFqcn, $format);
+                } catch (RendererNotFoundException $exception) {
+                    // TODO log this
+                }
             }
         }
     }
